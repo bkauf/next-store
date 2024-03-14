b55a1cd98a9e75edbf5e752968a96ea5c918a47b
Brian Kaufman <bkauf@google.com>
2024-03-14 16:57:03 +0000

    website changes

--- chatbot/app/api/gemini/route.ts
+++ chatbot/app/api/gemini/route.ts
@@ -2,9 +2,8 @@ import { NextResponse } from "next/server";
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const fs = require("fs");
 const https = require("https");
-const axios = require('axios');
+const axios = require("axios");
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-
 
 interface PostData {
   name: string;
@@ -15,9 +14,9 @@ export async function POST(request: Request) {
   const postData = await request.json(); // get request body
 
   const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
-  const prompt = "Write a short product description for the item in this image?";
+  const prompt = "Write a short product description for the item in this image";
 
-  let imgBase64 = await downloadFile(postData.fileName)
+  let imgBase64 = await downloadFile(postData.fileName);
   const result = await model.generateContent([
     prompt,
     postData.name,
@@ -31,47 +30,43 @@ export async function POST(request: Request) {
 }
 
 const getImage = async (fileName: string) => {
-  console.log("downloading image from GCS...")
-  const path = "image.jpg"
-  const mimeType = "image/jpeg"
+  console.log("downloading image from GCS...");
+  const path = "image.jpg";
+  const mimeType = "image/jpeg";
   const file = fs.createWriteStream("image.jpg");
   const request = https.get(
-    "https://storage.googleapis.com/hipster-ai-images/" + fileName,
+    `https://storage.googleapis.com/${process.env.GCS_BUCKET}/${fileName}`,
     function (response: any) {
       response.pipe(file);
-      console.log("downloading image complete...")
-  })
-   
+      console.log("downloading image complete...");
+    }
+  );
 };
 
 async function downloadFile(fileName: string) {
-  console.log("downloading image from GCS...")
+  console.log("downloading image from GCS...");
   try {
     const response = await axios({
-      url: "https://storage.googleapis.com/hipster-ai-images/" + fileName,
-      method: 'GET',
-      responseType: 'stream' // Important: stream the response
+      url: `https://storage.googleapis.com/${process.env.GCS_BUCKET}/${fileName}`,
+      method: "GET",
+      responseType: "stream", // Important: stream the response
     });
 
     const writer = fs.createWriteStream("image.jpg");
 
     response.data.pipe(writer); // Pipe the data to the write stream
-    console.log("Download Finished...")
+    console.log("Download Finished...");
     return new Promise((resolve, reject) => {
-      writer.on('finish', resolve);
-      writer.on('error', reject);
+      writer.on("finish", resolve);
+      writer.on("error", reject);
     });
-
   } catch (error) {
-    console.error("Error downloading file:", error); 
+    console.error("Error downloading file:", error);
   }
 }
 
-
-
-
 function fileToGenerativePart(path: string, mimeType: string) {
-  console.log("Convert to base64...")
+  console.log("Convert to base64...");
   return {
     inlineData: {
       data: Buffer.from(fs.readFileSync(path)).toString("base64"),
