e6d13f4a4c0fe29e79dc420113f7a1a3386140c2
Brian Kaufman <bkauf@google.com>
2024-03-25 15:23:06 +0000

    create product

--- demo-website/app/components/Menu.js
+++ demo-website/app/components/Menu.js
@@ -5,7 +5,7 @@ const Menu = () => {
     <div className="mb-2">
             <Link href="/" ><span className="font-bold text-blue-600"> View Products</span> </Link> |
 
-      <Link href="/product" ><span className="font-bold text-blue-600 m-2">Create Product</span> </Link> |
+      <Link href="/createproduct" ><span className="font-bold text-blue-600 m-2">Create Product</span> </Link> |
       <Link href="/search" ><span className="font-bold text-blue-600 m-2">Search</span> </Link> 
 
     </div>
