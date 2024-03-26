64e0fda786e497257b5c9b60f01da741b05d54f5
Brian Kaufman <bkauf@google.com>
2024-03-26 17:06:15 +0000

    working locally

--- demo-website/app/api/upload/route.ts
+++ demo-website/app/api/upload/route.ts
@@ -7,83 +7,60 @@ export async function POST(request: NextRequest) {
   const file: File | null = data.get("file") as unknown as File;
 
   if (!file) {
-    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 
   const bytes = await file.arrayBuffer();
   const buffer = Buffer.from(bytes);
 
-
   try {
     //await uploadToGCS(file, buffer);
     const { Storage } = require("@google-cloud/storage");
     const storage = new Storage();
-    console.log("Uploading file to GCS...")
-  
-    await storage
+    console.log("Uploading file to GCS...");
+
+   await storage
       .bucket(process.env.GCS_BUCKET)
       .file(file.name)
       .save(Buffer.from(buffer))
       .catch(console.error, console.log("file upload failed"))
       .then(() => {
         console.log("File uploaded to GCS");
-      
       });
 
 
-   
-
-
-    setTimeout(myFunction, 2000); // Delay of 2 seconds (2000 milliseconds)
-
-
-
-    console.log("File uploaded complete")
-    // return NextResponse.json({
-    //     link:
-    //       "https://storage.googleapis.com/" +
-    //       process.env.GCS_BUCKET +
-    //       "/" +
-    //       file.name,
-    //   })
+    console.log("File uploaded complete");
+    return NextResponse.json({
+        link:
+          "https://storage.googleapis.com/" +
+          process.env.GCS_BUCKET +
+          "/" +
+          file.name,
+      })
   } catch (error) {
     console.log(error);
-    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
-  } 
-
-  
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
 
-const uploadToGCS = async (file: any, buffer: any) => {
-  const { Storage } = require("@google-cloud/storage");
-  const storage = new Storage();
-  console.log("Uploading file to GCS...")
+// const uploadToGCS = async (file: any, buffer: any) => {
+//   const { Storage } = require("@google-cloud/storage");
+//   const storage = new Storage();
+//   console.log("Uploading file to GCS...");
 
-  let result = await storage
-    .bucket(process.env.GCS_BUCKET)
-    .file(file.name)
-    .save(Buffer.from(buffer))
-    .catch(console.error, console.log("file upload failed"))
-    .then(() => {
-      console.log("File uploaded to GCS");
-    
-    });
+//   let result = await storage
+//     .bucket(process.env.GCS_BUCKET)
+//     .file(file.name)
+//     .save(Buffer.from(buffer))
+//     .catch(console.error, console.log("file upload failed"))
+//     .then(() => {
+//       console.log("File uploaded to GCS");
+//     });
+// };
 
-    return NextResponse.json({
-        link:
-          "https://storage.googleapis.com/" +
-          process.env.GCS_BUCKET +
-          "/"})
-};
-
-
-
-function myFunction() {
-    console.log("This will be logged after 2 seconds");
-    return NextResponse.json({
-        link:
-          "https://storage.googleapis.com/" +
-          process.env.GCS_BUCKET +
-          "/" 
-      })
-}
